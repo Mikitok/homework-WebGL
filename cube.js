7a957c1c7e668c62a07b06afd07aed25cf0d6493
Mikito <978808462@qq.com>
2019-01-01 15:12:46 +0800

    newest

--- cube.js
+++ cube.js
@@ -28,13 +28,13 @@ function main() {
 
 function initScene(){
     scene=new THREE.Scene();
-    scene.fog = new THREE.FogExp2(0xd0e0f0, 0.0025);
+    scene.fog = null;
 
-    var path = '../homework/img/';
+    var path = '../img/';
     var urls = [
         path + 'right.jpg', path + 'left.jpg',
         path + 'top.jpg', path + 'bottom.jpg',
-        path + 'front.jpg', path + 'back.jpg'
+        path +'back.jpg' , path + 'front.jpg'
     ];
     var textureCube = new THREE.CubeTextureLoader().load( urls );
     scene.background = textureCube;
@@ -62,25 +62,27 @@ function initCamera() {
 
 function initLight() {
     // 环境光，(color, groundcolor, intensity, position, visible)
-    light=new THREE.HemisphereLight(0xfffff0, 0x101020, 1.25);
-    light.position.set(0.75, 1, 0.25);
+    //light=new THREE.HemisphereLight(0xfffff0, 0x101020, 1.25);
+    //light.position.set(0.75, 1, 0.25);
+    light=new THREE.AmbientLight( 0x404040 );
+    // light.castShadow=true;
     scene.add(light);
 
-    var spotLight=new THREE.SpotLight(0xffffff);
-    spotLight.position.set(0, 100, 15);
+    var spotLight=new THREE.SpotLight(0xffffcc);
+    spotLight.position.set(0, 200, 100);
     spotLight.castShadow=true;
     scene.add(spotLight);
 
 }
 
 function initObject() {
-    var material = new THREE.MeshLambertMaterial({ color: 0x8B4513 });
-    var geometry = new THREE.PlaneGeometry(2000, 2000);
-    plane = new THREE.Mesh(geometry, material);
-    plane.rotation.x = (-90 * Math.PI) / 180;
-    //plane.castShadow=true;
-    plane.receiveShadow=true;
-    scene.add(plane);
+    // var material = new THREE.MeshLambertMaterial({ color: 0x8B4513 });
+    // var geometry = new THREE.PlaneGeometry(2000, 2000);
+    // plane = new THREE.Mesh(geometry, material);
+    // plane.rotation.x = (-90 * Math.PI) / 180;
+    // //plane.castShadow=true;
+    // plane.receiveShadow=true;
+    // scene.add(plane);
 
     city=new THREEx.ModernCity();
     city.castShadow=true;
